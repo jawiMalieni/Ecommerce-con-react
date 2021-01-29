f417daa9e15e2879d229a2ac42460e434f8b621f
Javier H Malieni <casamalieni@gmail.com>
2021-01-28 21:52:21 -0300

    enu

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/navbar';
+const App = () => {
+  return (
+    <>
+      <NavbarComponent />
+
+      <h1 className="title">Milla Electronicts</h1>
+
+    <h2>Encontra los mejores productos de electronica</h2>
+
+
+    </>
+  );
+}
+
+export default App;

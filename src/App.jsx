ff05cb9929f0efbf7bafddce4bf9d5939a1fa02a
Javier H Malieni <casamalieni@gmail.com>
2021-02-03 00:12:48 -0300

    estructura components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
+import React, { Component } from 'react';
+import {Provider} from 'react-redux';
+import {createStore } from 'redux';
+import rootReducer from './reducers';
+
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+
 import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComponent from './components/navbar';
-const App = () => {
-  return (
-    <>
-      <NavbarComponent />
-
-      <h1 className="title">Milla Electronicts</h1>
-
-    <h2>Encontra los mejores productos de electronica</h2>
+import Home from "./pages/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import ProductDetail from "./pages/ProductDetail/ProductDetail";
+import ShoppingCart from "./pages/ShopingCart/ShoppingCart";
 
 
-    </>
-  );
+
+export const  store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+class App extends Component {
+  render() {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+            <React.Fragment>
+                <Header/>
+                <Switch>
+                    <Route exact path={'/'} render={() => {
+                        return <Redirect to={'/products'}/>
+                    }}/>
+                    <Route exact path={'/products'} component={Home}/>
+                    <Route exact path={'/products/:id'} component={ProductDetail}/>
+                    <Route exact patr={'/cart'} component={ShoppingCart}/>
+                </Switch>
+                <Footer/>
+            </React.Fragment>
+            </BrowserRouter>
+        </Provider>
+    );
+  }
 }
 
 export default App;
